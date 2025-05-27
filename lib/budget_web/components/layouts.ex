bcea4374e9d1af22c4999200bf903c2074d6b21d
Raul Pereira <raul.pereira@gmail.com>
2025-05-26 23:27:42 -0300

    init project

---
+++ lib/budget_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule BudgetWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use BudgetWeb, :controller` and
+  `use BudgetWeb, :live_view`.
+  """
+  use BudgetWeb, :html
+
+  embed_templates "layouts/*"
+end
